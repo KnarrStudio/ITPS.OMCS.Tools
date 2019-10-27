c78dd72f2cb15057d19924cff99003c36a2bb0d6
Erik Arnesen <earnesen@ymail.com>
2019-10-27 00:50:06 -0400

    Create Start-DailyChecks.ps1

---
+++ Start-DailyChecks.ps1
@@ -0,0 +1,24 @@
+﻿#requires -Version 2.0 -Modules ITPS.OMCS.Tools
+function Start-DailyChecks
+{
+  <#
+    .SYNOPSIS
+    Short Description
+    .DESCRIPTION
+    Detailed Description
+    .EXAMPLE
+    Start-DailyChecks
+    explains how to use the command
+    can be multiple lines
+    .EXAMPLE
+    Start-DailyChecks
+    another example
+    can have as many examples as you like
+  #>
+  Clear-Host
+  Test-FiberSatellite -Sites www.yahoo.com
+  Test-PrinterStatus -PrintServer Printserver -PingReportFolder \\fileshare
+  Test-AdWorkstationConnections -ADSearchBase xxx -PingReportFolder \\fileshare -OutputFileName WorkstationReport
+  Import-Csv -Path WorkstationList | Get-UpTime -ShowOfflineComputers -DisplayOnly
+}
+
