24e27086e8f6154dd3053346563f9dd1201ac4ef
Erik Arnesen <earnesen@ymail.com>
2019-12-15 11:19:54 -0500

    Update Test-Replication.ps1

--- Scripts/Test-Replication.ps1
+++ Scripts/Test-Replication.ps1
@@ -7,24 +7,32 @@
       .DESCRIPTION
       Perform a user based test to ensure Replication is working.  You must know at least two of the replication partners
 
+      .PARAMETER FilePath
+      Name of the file to test.  Format as a txt file with starting with the '\'
+
       .PARAMETER DfsrServers
       Two or more of the replication partner's Net Bios Name
 
-      .PARAMETER TestFile
-      Name of the file to test.  Format as a txt file with starting with the '\'
+      .PARAMETER test
+      Changes sleep time to 1 second.  
 
       .EXAMPLE
-      Test-Replication -DfsrServers Server1, Server2, Server3 -TestFile \folder-1\test-date.txt
+      .EXAMPLE
+      Test-Replication -DfsrServers Server1, Server2, Server3 -FilePath  \folder-1\test-date.txt
     
-      Name                           Value                                                                                      
-      ----                           -----                                                                                      
-      Server1                       12/14/2019 11:06:29 - Good                                                                 
-      Server3                       12/14/2019 11:06:29 - File Missing                                                         
-      Server2                       12/14/2019 11:06:30 - Failed                                                      
+      Name              Value
+      ----              -----
+      Server1           12/14/2019 11:06:29 - Good
+      Server3           12/14/2019 11:06:29 - File Missing
+      Server2           12/14/2019 11:06:30 - Failed
 
       Good: The file has been replicated 
       Failed: The file has not replicated 
       File Missing: is just that
+
+      The file contents will look like:
+      12/15/2019 10:01:00 - MyUserName Tested replication of this file from  Workstation1
+      12/15/2019 10:03:48 - MyUserName Tested replication of this file from  Workstation1
 
       .NOTES
       Place additional notes here.
@@ -34,90 +42,113 @@
       The first link is opened by Get-Help -Online Test-Replication
 
       .INPUTS
-      List of Server names a
+      List of Server names and file path
 
       .OUTPUTS
-      List of output types produced by this function.
+      Screen and file
   #>
-
 
   [CmdletBinding()]
   param
   (
+    [Parameter(Mandatory ,HelpMessage = 'Enter a file and path name.  Example "\Sharename\Filename.log"')]
+    #Reserve for PS ver 6 -     [ValidatePattern('(\\[a-zA-Z0-9\-_]{1,}){1,}[\$]{0,1}',ErrorMessage = 'The pattern needs to be \Sharename\Filename')]
+    [ValidatePattern('(\\[a-zA-Z0-9\-_]{1,}){1,}[\$]{0,1}')]
+    [String]$FilePath,
     [Parameter(Mandatory,HelpMessage = 'DFSR path to test files separated by comma', Position = 0)]
     [ValidateCount(2,5)]
     [String[]]$DfsrServers,
-    [Parameter(Mandatory ,HelpMessage = 'Enter a filename with parent folder ex. "\folder-1\test-date.txt"')]
-    [ValidatePattern({
-          \\*\\*.txt
-    })]
-    [String]$TestFile #= '\folder-1\test-date.txt'
+    [Switch]$test
   )
-
-  $FirstDfsrServer = $DfsrServers[0] 
+   
+  $Results = [ordered]@{}
+  $FirstDfsrServer = $DfsrServers[0]
+  $TestFilePath = ('\\{0}{1}' -f $FirstDfsrServer, $FilePath)   
   
-  $results = [ordered]@{}
-  $instructions = "`n Good: The file has been replicated `n Failed: The file has not replicated `n File Missing: is just that"
-  
-  function Get-TdStamp 
+  if(-not $test)
   {
-    #Possible future use
+    $time = 5
+    $instructions = "`n Good: The file has been replicated `n Failed: The file has not replicated `n File Missing: is just that"
   }
+  else
+  {
+    $time = 1
+    $instructions = 'TEST MODE'
+  }
+ 
+  
+  function Get-TimeStamp 
+  {
+    $(Get-Date -Format G)
+  }
+  Write-Debug -Message ('Time: {0}' -f $time)
   
   $DateTime = Get-Date -Format G
-  $DateTimeUserStamp = ('{0} - {1} Tested replication of this file from the computer {2}' -f $DateTime, $env:username, $env:COMPUTERNAME)
-  $DateTimeUserStamp | Out-File -FilePath ('\\{0}{1}' -f $FirstDfsrServer, $TestFile) -Append
+  Write-Debug -Message ('Date Time: {0}' -f $DateTime)
+  
+  $DateTimeUserStamp = ('{0} - {1} Tested replication of this file from {2}' -f $DateTime, $env:username, $env:COMPUTERNAME)
+  Write-Debug -Message ('Date Time User Stamp: {0}' -f $DateTimeUserStamp)
+  
+  #$DateTimeUserStamp = ('{0} - {1}' -f $DateTime, $env:username)
+  $DateTimeUserStamp | Out-File -FilePath $TestFilePath  -Append
 
   #Only for Testing.  Comment ou for real script
   #Copy-Item -Path '.\Server-1\test-date.txt' -Destination '.\Server-2\test-date.txt'
   
   foreach($Server in $DfsrServers)
   {
-    $ServerShareFile = ('\\{0}{1}' -f $Server, $TestFile)
+    Write-Debug -Message ('Server:  {0}' -f $Server)
+    Write-Debug -Message ('File path: {0}' -f $FilePath)
+
+    $ServerShareFile = ('\\{0}{1}' -f $Server, $FilePath)
+    Write-Debug -Message ('Server Share File: {0}' -f $ServerShareFile)
 
     If(Test-Path -Path $ServerShareFile)
     {
       #Sleep to give the file time to replicate
-      Start-Sleep -Seconds 3
+      Start-Sleep -Seconds $time
 
       if($(Get-Content -Path $ServerShareFile  | Select-String -Pattern $DateTimeUserStamp))
       {
-        $results.Add($Server,$("$(Get-Date -Format G) - Good"))
+        $TimeStamp = Get-TimeStamp
+        $Results.Add($Server,$(('{0} - Good' -f $TimeStamp)))
       }
       else
       {
-        Start-Sleep -Seconds 10
+        Start-Sleep -Seconds $time*2
 
         if($(Get-Content -Path $ServerShareFile  | Select-String -Pattern $DateTimeUserStamp))
         {
-          $results.Add($Server,$("$(Get-Date -Format G) - Good"))
+          $TimeStamp = Get-TimeStamp
+          $Results.Add($Server,$(('{0} - Good' -f $TimeStamp)))
         }
         else
         {
-          $results.Add($Server,$("$(Get-Date -Format G) - Failed"))
+          $TimeStamp = Get-TimeStamp
+          $Results.Add($Server,$(('{0} - Failed' -f $TimeStamp)))
         }
       }
     }
     Else 
     {
-      $results.Add($Server,$("$(Get-Date -Format G) - File Missing"))
+      $TimeStamp = Get-TimeStamp
+      $Results.Add($Server,$(('{0} - File Missing' -f $TimeStamp)))
     }
   }
 
-  $results
-  Start-Sleep -Seconds 1
+  $Results
   Write-Output -InputObject ('{0}' -f $instructions )
 }
 
-#Test-Replication -DfsrServers Lenova-11, Server2 -TestFile \folder-1\test-date.txt
 
+#Test-Replication -DfsrServers 'Localhost', '$env:computername' -FilePath \folder-1\test-date.txt
 
 
 # SIG # Begin signature block
 # MIID7QYJKoZIhvcNAQcCoIID3jCCA9oCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUzOGuc2Sg8aid5P8vmAYbwNFE
-# ziugggINMIICCTCCAXagAwIBAgIQyWSKL3Rtw7JMh5kRI2JlijAJBgUrDgMCHQUA
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUYRPFiXt2Ya0LQh9wNSoz40JK
+# 56SgggINMIICCTCCAXagAwIBAgIQyWSKL3Rtw7JMh5kRI2JlijAJBgUrDgMCHQUA
 # MBYxFDASBgNVBAMTC0VyaWtBcm5lc2VuMB4XDTE3MTIyOTA1MDU1NVoXDTM5MTIz
 # MTIzNTk1OVowFjEUMBIGA1UEAxMLRXJpa0FybmVzZW4wgZ8wDQYJKoZIhvcNAQEB
 # BQADgY0AMIGJAoGBAKYEBA0nxXibNWtrLb8GZ/mDFF6I7tG4am2hs2Z7NHYcJPwY
@@ -131,9 +162,9 @@
 # fJ/uMYIBSjCCAUYCAQEwKjAWMRQwEgYDVQQDEwtFcmlrQXJuZXNlbgIQyWSKL3Rt
 # w7JMh5kRI2JlijAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKA
 # ADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYK
-# KwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQU3x6efl/os/GpWTZgI3dLSZH0fh8w
-# DQYJKoZIhvcNAQEBBQAEgYCCO8HxFkQl4cUppEDUDZ4cvpVUJFwGGwYDqTGkD41u
-# VVQ4COGoJ5tl91NhWMBRZ08/nZEN9GOAevKbIY5n5Gl1wPU6zV06CeVZfSHvF+nF
-# 6zRIi5NkIvgQEceAKcsvoyU1gktj4LDGs9SyQSp9BP3RZKHWkKg9CeMjEG34IL5f
-# ZA==
+# KwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQU1X89O77GUVdCq34fJqXbXHphvpww
+# DQYJKoZIhvcNAQEBBQAEgYBS12uEKDerCZuG7BTPq2TJSsyjGPby6YbL4wI0XryK
+# Vyzinmakk66D8Ivz0aoeTcvcaarjLqgTbV+06D5D9JzhJDVXzdhAi7xMXqWvSogD
+# irHM/3UEs4xJV57vqb2z1na3r2sD2W9Oj/bxPoeS0BtMU/xPeGhcUG9jHdX2Dgue
+# qQ==
 # SIG # End signature block
