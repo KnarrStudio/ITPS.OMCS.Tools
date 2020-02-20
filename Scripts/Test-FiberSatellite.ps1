871e8d2624fad42eb699aba20f0b43d12a6bbc4f
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-02-19 21:41:47 -0500

    Update Test-FiberSatellite.ps1

--- Scripts/Test-FiberSatellite.ps1
+++ Scripts/Test-FiberSatellite.ps1
@@ -1,11 +1,57 @@
 #requires -Version 3.0 -Modules NetTCPIP
 function Test-FiberSatellite
 {
+  <#
+      .SYNOPSIS
+      "Pings" a group of sites or servers and gives a response in laymans terms.
+
+      .DESCRIPTION 
+      "Pings" a group of sites or servers and gives a response in laymans terms. 
+      This started due to our need to find out if transport was over fiber or bird.  
+      There are some default remote sites that it will test, but you can pass your own if you only want to check one or two sites. 
+
+      .PARAMETER Sites
+      A single or list of sites or servers that you want to test against.
+
+      .PARAMETER Simple
+      Provides a single output line for those who just need answers.
+
+      .PARAMETER Log
+      Sends the output to a log file.
+
+      .PARAMETER ReportFolder
+      The folder where the output log will be sent.
+
+      .EXAMPLE
+      Test-FiberSatellite -Sites Value
+    
+      .EXAMPLE
+      Test-FiberSatellite -Simple
+      Creates a simple output
+
+      .EXAMPLE
+      Test-FiberSatellite -Log -ReportFolder Value
+      Sends the log to the folder identified.
+
+      .LINK
+      URLs to related sites
+      The first link is opened by Get-Help -Online Test-FiberSatellite
+
+      .INPUTS
+      List of input types that are accepted by this function.
+
+      .LINK
+      https://github.com/KnarrStudio/ITPS-Tools/wiki
+
+      .OUTPUTS
+      To console or screen at this time.
+  #>
+
   <#PSScriptInfo
 
-      .VERSION 2.0
+      .VERSION 2.1
 
-      .GUID c528ef9f-ccdf-47ca-8885-8883598c5e79
+      .GUID ac39aa3a-ea05-433f-af82-21925b2af50b
 
       .AUTHOR Erik
 
@@ -17,7 +63,7 @@ function Test-FiberSatellite
 
       .LICENSEURI
 
-      .PROJECTURI
+      .PROJECTURI  https://github.com/KnarrStudio/ITPS-Tools/wiki
 
       .ICONURI
 
@@ -33,35 +79,6 @@ function Test-FiberSatellite
       .PRIVATEDATA
 
   #>
-  <# 
-
-      .SYNOPSIS
-      "Pings" a group of sites or servers and gives a response in laymans terms.
-
-      .DESCRIPTION 
-      "Pings" a group of sites or servers and gives a response in laymans terms. 
-      This started due to our need to find out if transport was over fiber or bird.  
-      There are some default remote sites that it will test, but you can pass your own if you only want to check one or two sites. 
-
-      .PARAMETER Sites
-      A single or list of sites or servers that you want to test against.
-
-      .PARAMETER Simple
-      Provides a single output line for those who just need answers.
-
-      .EXAMPLE
-      Test-FiberSatellite -Sites Value -Simple
-      Tests the Value and displays the output as a single line of text
-
-      .NOTES
-      Place additional notes here.
-
-      .LINK
-      https://github.com/KnarrStudio/ITPS-Tools/wiki
-
-      .OUTPUTS
-      To console or screen at this time.
-  #>
 
   [cmdletbinding(DefaultParameterSetName = 'Default')]
   param
@@ -74,7 +91,6 @@ function Test-FiberSatellite
     [Switch]$Log,
     [Parameter (ParameterSetName = 'Log')]
     [String]$ReportFolder = "$env:temp\Reports\FiberSatellite"
-
   )
   
   $ReportList = [Collections.ArrayList]@()
@@ -168,5 +184,36 @@ function Test-FiberSatellite
 
 # For Testing:
 #Test-FiberSatellite
+#Test-FiberSatellite -Sites localhost,'yahoo.com'
+#Test-FiberSatellite -Sites localhost,'yahoo.com' -Simple 
+#Test-FiberSatellite -Sites localhost,'yahoo.com' -Simple -Verbose
+#Test-FiberSatellite -Log -ReportFolder C:\Temp
+#Test-FiberSatellite -Log -Verbose
 
 
+
+
+# SIG # Begin signature block
+# MIID/AYJKoZIhvcNAQcCoIID7TCCA+kCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUosLCnPROISOz3fSiIutFLdqu
+# DKagggIRMIICDTCCAXagAwIBAgIQapk6cNSgeKlJl3aFtKq3jDANBgkqhkiG9w0B
+# AQUFADAhMR8wHQYDVQQDDBZLbmFyclN0dWRpb1NpZ25pbmdDZXJ0MB4XDTIwMDIx
+# OTIyMTUwM1oXDTI0MDIxOTAwMDAwMFowITEfMB0GA1UEAwwWS25hcnJTdHVkaW9T
+# aWduaW5nQ2VydDCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAxtuEswl88jvC
+# o69/eD6Rtr5pZikUTNGtI2LqT1a3CZ8F6BCC1tp0+ftZLppxueX/BKVBPTTSg/7t
+# f5nkGMFIvbabMiYtfWTPr6L32B4SIZayruDkVETRH74RzG3i2xHNMThZykUWsekN
+# jAer+/a2o7F7G6A/GlH8kan4MGjo1K0CAwEAAaNGMEQwEwYDVR0lBAwwCgYIKwYB
+# BQUHAwMwHQYDVR0OBBYEFGp363bIyuwL4FI0q36S/8cl5MOBMA4GA1UdDwEB/wQE
+# AwIHgDANBgkqhkiG9w0BAQUFAAOBgQBkVkTuk0ySiG3DYg0dKBQaUqI8aKssFv8T
+# WNo23yXKUASrgjVl1iAt402AQDHE3aR4OKv/7KIIHYaiFTX5yQdMFoCyhXGop3a5
+# bmipv/NjwGWsYrCq9rX2uTuNpUmvQ+0hM3hRzgZ+M2gmjCT/Pgvia/LJiHuF2SlA
+# 7wXAuVRh8jGCAVUwggFRAgEBMDUwITEfMB0GA1UEAwwWS25hcnJTdHVkaW9TaWdu
+# aW5nQ2VydAIQapk6cNSgeKlJl3aFtKq3jDAJBgUrDgMCGgUAoHgwGAYKKwYBBAGC
+# NwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgor
+# BgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUTmsVK4rd
+# 367SGscB8bGcAXvWpX0wDQYJKoZIhvcNAQEBBQAEgYAkGcyKEXcfDg5RPpystg7Y
+# d6G7Gx+DRAt30QnNQAtKfu8RNFeuUlhaSGwQjHvs/ykslBGNhBqhFT0vTH5eFBT9
+# 3QF9WzcFR4B5w2G/XZSo9vKyrmfhxjpubnLBS7g8Aa2xF9PxCAAkUZ9+8iGDM3VL
+# GSgC01zigZ+K4g8U7zjIIw==
+# SIG # End signature block
