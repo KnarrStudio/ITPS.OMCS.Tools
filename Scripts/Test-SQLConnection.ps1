51d4091636f4d1a7104ba5e3f0987cc4bc3736be
Erik Arnesen <earnesen@ymail.com>
2020-04-12 06:08:40 -0400

    Function creates the file now
    Added a param for the file path, if that param is presen, it creates the file too.

--- Scripts/Test-SQLConnection.ps1
+++ Scripts/Test-SQLConnection.ps1
@@ -1,30 +1,77 @@
 ﻿function Test-SQLConnection
-{    
-    [OutputType([bool])]
-    Param
-    (
-        [Parameter(Mandatory,HelpMessage='Add SQL ServerNAme',
-                    ValueFromPipelineByPropertyName,
-                    Position=0)]
-        [String]$ConnectionString
-    )
-    try
+{
+  <#
+      .SYNOPSIS
+      Describe purpose of "Test-SQLConnection" in 1-2 sentences.
+
+      .DESCRIPTION
+      Add a more complete description of what the function does.
+
+      .PARAMETER ConnectionString
+      Describe parameter -ConnectionString.
+
+      .EXAMPLE
+      Test-SQLConnection -ConnectionString Value
+      Describe what this call does
+
+      .NOTES
+      Place additional notes here.
+
+      .LINK
+      URLs to related sites
+      The first link is opened by Get-Help -Online Test-SQLConnection
+
+      .INPUTS
+      List of input types that are accepted by this function.
+
+      .OUTPUTS
+      List of output types produced by this function.
+
+      $TimeStamp = Get-Date -Format G 
+
+
+  #>
+
+    
+  [OutputType([bool])]
+  Param
+  (
+    [Parameter(Mandatory,HelpMessage = 'Add SQL ServerNAme',
+        ValueFromPipelineByPropertyName,Position = 0)]
+    [String]$ConnectionString,
+    [Parameter (ParameterSetName = 'Log')]
+    [Switch]$Log,
+    [Parameter (ParameterSetName = 'Log')]
+    [String]$ReportFolder = "$env:temp\Reports\SqlConnection"
+  )
+  try
+  {
+    $sqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection -ArgumentList $ConnectionString
+
+    $sqlConnection.Open()
+
+    $sqlConnection.Close()
+
+
+    return $true
+  }
+  catch
+  {
+    return $false
+  }
+  If($Log)
+  {
+  $TimeStamp = Get-Date -Format G 
+    If(-not (Test-Path -Path $ReportFolder))
     {
-        $sqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection -ArgumentList $ConnectionString
-
-        $sqlConnection.Open()
-
-        $sqlConnection.Close()
-
-
-        return $true
-
+      New-Item -Path $ReportFolder -ItemType Directory
     }
-    catch
-    {
-        return $false
-
-    }
+    $OutputTable.Report | Out-File -FilePath $ReportFile -Append
+    $ReportList | Out-File -FilePath $ReportFile -Append
+    ('-' * 30) | Out-File -FilePath $ReportFile -Append
+    Write-Output -InputObject ('You can find the full report at: {0}' -f $ReportFile)
+    Start-Process -FilePath notepad -ArgumentList $ReportFile
+  }
 }
 
 Test-SQLConnection
