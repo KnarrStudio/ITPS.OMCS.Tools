11ca8958be099e208f57bf0b9655c0413c92b47f
Erik Arnesen <earnesen@ymail.com>
2020-04-11 22:16:36 -0400

    Create Test-SQLConnection.ps1

---
+++ Scripts/Test-SQLConnection.ps1
@@ -0,0 +1,30 @@
+﻿function Test-SQLConnection
+{    
+    [OutputType([bool])]
+    Param
+    (
+        [Parameter(Mandatory,HelpMessage='Add SQL ServerNAme',
+                    ValueFromPipelineByPropertyName,
+                    Position=0)]
+        [String]$ConnectionString
+    )
+    try
+    {
+        $sqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection -ArgumentList $ConnectionString
+
+        $sqlConnection.Open()
+
+        $sqlConnection.Close()
+
+
+        return $true
+
+    }
+    catch
+    {
+        return $false
+
+    }
+}
+
+Test-SQLConnection
