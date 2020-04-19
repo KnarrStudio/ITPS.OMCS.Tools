bb9b51fcae249a4cf8dd9405a50f5ac07e579c83
Erik Arnesen <earnesen@ymail.com>
2020-04-18 22:40:13 -0400

    Added test for file change

--- Start-DailyChecks.ps1
+++ Start-DailyChecks.ps1
@@ -1,4 +1,4 @@
-﻿#requires -Version 3.0 -Modules ITPS.OMCS.Tools
+﻿#requires -Version 4.0 
 <#
     .SYNOPSIS
     Use this to run the daily checks.  
@@ -12,40 +12,90 @@
 
 #>
 
+param
+(
+  [Parameter(Position = 0)]
+  [Switch]$ResetFiles
+)
 
 # Finding localpath of script
 # $scriptDir = $PSScriptRoot
 
 # Setting the file and path to a variable
-#$MyLocalParameters = "$PSScriptRoot\MyLocalParameters.txt"
-$MyLocalParameters = ".\MyLocalParameters.txt"
+# $MyLocalParameters = "$PSScriptRoot\MyLocalParameters.txt"
 
-if($erik -ne 1)
+$MyLocalParameters = 'MyLocalParameters.txt'
+Write-Verbose -Message ('Local Parameter Path: {0}' -f $MyLocalParameters)
+
+$OriginalFile = 'MyLocalParameters.org'
+Write-Verbose -Message ('Original File: {0}' -f $OriginalFile)
+
+if($ResetFiles)
 {
-  Write-Host "Edit the '$MyLocalParameters' file" -ForegroundColor Cyan
-  Return  # To run, delete this line of code.
+  Write-Verbose -Message ('Resetting the {0} back to default {1}' -f $MyLocalParameters, $OriginalFile)
+  Get-ChildItem |
+  Where-Object -FilterScript {
+$_.BaseName -match ($OriginalFile.Split('.')[0])
+} |
+  ForEach-Object -Process {
+Remove-Item -Path $_
 }
- 
-Invoke-Expression -Command (Get-Content $MyLocalParameters | Out-String )
-Clear-Host
+  $HashFile = Get-ChildItem -Path '*.hash' |
+  Sort-Object -Property LastWriteTime |
+  Select-Object -First 1
+  Rename-Item -Path $HashFile -NewName $OriginalFile
 
+  Write-Verbose -Message ('Reset files completed')
+  Start-Sleep -Seconds 3
+  Return
+} 
 
-If(Test-FiberSatellite)
+if(-not (Test-Path -Path $MyLocalParameters))
 {
+  Write-Verbose -Message ('{0} file does not exist. ' -f $MyLocalParameters)
+
+  $Null = Copy-Item -Path $OriginalFile -Destination $MyLocalParameters
+  Write-Verbose -Message ('{0} copied to {1}' -f $OriginalFile, $MyLocalParameters)  
+  
+  $HashName = ('{0}.hash' -f (Get-FileHash -Path $MyLocalParameters).hash)
+  $Null = Rename-Item -Path ('{0}' -f $OriginalFile) -NewName $HashName
+  Write-Verbose -Message ('Renaming the {0} to {1}' -f $OriginalFile, $HashName)
+}
+
+$CurrentHash = (Get-FileHash -Path $MyLocalParameters).hash
+Write-Verbose -Message ('Retrieving the current hash of the {0}' -f $MyLocalParameters)
+Write-Verbose -Message ('Current Hash: {0}' -f $CurrentHash)
+
+if(Test-Path -Path "$CurrentHash.hash")
+{
+  Write-Verbose -Message ('The Test to see if {0} has been modified failed.' -f $MyLocalParameters)
+  #Clear-Host
+  Write-Output  -InputObject ("Edit the '{0}' file for your environment" -f $MyLocalParameters) 
+  #Return
+}
+else
+{
+    Invoke-Expression -Command (Get-Content -Path $MyLocalParameters | Out-String )
+    Clear-Host
+
+    If(Test-FiberSatellite)
+    {
 Test-FiberSatellite @FiberSatellite
 }
  
-If($InstalledSoftware)
-{
-  Get-InstalledSoftware @InstalledSoftware
+    If($InstalledSoftware)
+    {
+Get-InstalledSoftware @InstalledSoftware
 }
 
-If($PrinterStatus)
-{
-  Test-PrinterStatus @PrinterStatus
+    If($PrinterStatus)
+    {
+Test-PrinterStatus @PrinterStatus
 }
   
-Test-AdWorkstationConnections @AdWorkstationConnections
-#Import-Csv -Path WorkstationList | Get-UpTime -ShowOfflineComputers -DisplayOnly
-
-
+        If($AdWorkstationConnections)
+    {
+      Test-AdWorkstationConnections @AdWorkstationConnections
+      #Import-Csv -Path WorkstationList | Get-UpTime -ShowOfflineComputers -DisplayOnly
+    }
+    }
