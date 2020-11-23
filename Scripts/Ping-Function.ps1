c30bab2c3711e104e7259048cc1ce7c02a48d21b
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-11-22 20:55:10 -0500

    Create the three modules
    to create a new version of the test-fiber script

---
+++ Scripts/Ping-Function.ps1
@@ -0,0 +1,112 @@
+﻿#requires -Version 3.0
+Function Ping-Function
+{
+  <#
+      .SYNOPSIS
+      Does the Ping work. This should hopefully be modular enough to take to the other "Ping scripts"
+  #>
+  param
+  (
+    [Parameter(Position = 0)]
+    [String[]] $Sites = ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com')
+  )
+  Begin {
+    # Set Variables  
+    $RttTotal = $NotRight = 0
+    $TotalResponses = $TotalSites = $Sites.Count 
+    $TimeStamp = Get-Date -Format G 
+    $ReportList = [Collections.ArrayList]@()
+    $PingHTList = [Ordered]@{
+      'DateStamp' = $TimeStamp
+    }
+    
+    function Get-AverageRTT 
+    {
+      <#
+          .SYNOPSIS
+          Returns the average response times by calculating the information in the $ReceivedJob object
+      #>
+      param
+      (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [Object]$ReceivedJob
+      )
+      $RttTotal = $null
+      ForEach($SiteJob in $ReceivedJob)
+      {
+        $RttTotal += $SiteJob.ResponseTime
+      }
+      [float]$RttTotal / $ReceivedJob.Count
+    }
+    
+    function Show-PingStatus 
+    {
+      <#
+          .SYNOPSIS
+          Translates the "Status Code" to something meaningful for the user
+      #>
+      param([Parameter(Mandatory = $true)]
+      [int]$InputObject)
+      switch ($InputObject)
+      {      
+        0  
+        {
+          'Success'
+        }
+        11003   
+        {
+          'Destination Host Unreachable'
+        }
+        11050   
+        {
+          'General Failure'
+        }
+        Default 
+        {
+          'Unknown Issue'
+        }
+      }
+    }
+  } # End Begin
+  Process {
+    # Starts the actual "Ping Job" here.  
+    # Then it will receive the job after waiting for it to be completed.  
+    # Removes the jobs from memory
+    $pingjob = Test-Connection -ComputerName $Sites -AsJob -Count 1
+    $ReceivedJob = Receive-Job -Job $pingjob -Wait
+    Remove-Job -Job $pingjob
+
+    # Works on the $ReceivedJob to get the data
+    # Adds it to a hash table
+    # Converts the hash table to an Object
+    ForEach($site in $ReceivedJob)
+    {
+      if($site.StatusCode -ne 0)
+      {
+        $TotalResponses = $TotalResponses - 1
+        $NotRight = $NotRight + 1
+      }
+      
+      Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 7 ),'$PingHTList') 
+      $PingHTList.DateStamp = [String]$TimeStamp
+      $PingHTList.Site = [String]$site.Address
+      $PingHTList.IpAddress = [String]$site.IPV4Address  
+      $PingHTList.ResponseTime = [String]$site.ResponseTime
+      $PingHTList.PingResult = (Show-PingStatus -InputObject ($site.StatusCode))
+      
+      Write-Verbose -Message ('{0} - {1} - {2}' -f $site.Address, $site.ResponseTime, $site.StatusCode)
+      
+      # Converts the hash table to object
+      Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 7 ),'$PingHTList') 
+      $null = $ReportList.Add([pscustomobject]$PingHTList)
+      
+    } # End ForEach
+  } # End Process
+  End {
+    $ReportList
+    }
+}
+
+#Testing Below
+$Sites = ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com')
+$Pingresult = Ping-Function -Sites $Sites -Verbose
--- Scripts/Test-FiberSatelliteJob.ps1
+++ Scripts/Test-FiberSatelliteJob.ps1
@@ -1,221 +1,303 @@
-﻿#requires -Version 4.0 
-function Test-FiberSatellite
+﻿function Test-FiberSatellite
 {
+  <#
+      .SYNOPSIS
+      "Pings" a group of sites or servers and gives a response in laymans terms.
+
+      .DESCRIPTION 
+      "Pings" a group of sites or servers and gives a response in laymans terms. 
+      This started due to our need to find out if transport was over fiber or bird.  
+      There are some default remote sites that it will test, but you can pass your own if you only want to check one or two sites. 
+
+      .PARAMETER Sites
+      A single or list of sites or servers that you want to test against.
+
+  #>
+   
   [cmdletbinding(DefaultParameterSetName = 'Default')]
   param
   (
     [Parameter(Position = 0)]
-    [String[]] $Sites = ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com'),
-    [Parameter (ParameterSetName = 'Default')]
-    [Switch]$Simple,
-    [Parameter (ParameterSetName = 'Log')]
-    [Switch]$Log,
-    [Parameter (ParameterSetName = 'Log')]
-    [String]$ReportFile = "$env:SystemDrive\temp\Reports\FiberSatellite\FiberSatellite.log",
-    [Parameter(Mandatory,HelpMessage = 'CSV file that is used for trending',Position = 1,ParameterSetName = 'Log')]
-    [String]$ReportCsv
+    [String[]] $Sites = ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com')
+    <#    ,
+        [Parameter (ParameterSetName = 'Log')]
+        [String]$ReportFile = "$env:SystemDrive\temp\Reports\FiberSatellite\FiberSatellite.log",
+        [Parameter(Mandatory=$false ,HelpMessage = 'CSV file that is used for trending',Position = 1,ParameterSetName = 'Log')]
+        [String]$ReportCsv
+    #>
   )
-  Begin {  
-    #region Initial Setup
-    $LineFormatText = 'Line {0}:  {1}'
-    function Get-AverageRTT
-{
-  [CmdletBinding()]
-  param
-  (
-    [Parameter(Mandatory = $false, Position = 0)]
-    $ReceivedJob
-  )
-  $r = 0
-  (1..$($ReceivedJob.Count)).ForEach({
-      $r = $r+($ReceivedJob[$_].ResponseTime)
-  })
-  $r/$ReceivedJob.Count
-}
+  
+  
+  #requires -Version 3.0
+  Function Ping-Function
+  {
+    <#
+        .SYNOPSIS
+        Does the Ping work. This should hopefully be modular enough to take to the other "Ping scripts"
+    #>
+    param
+    (
+      [Parameter(Position = 0)]
+      [String[]] $Sites = ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com')
+    )
+    Begin {
+      # Set Variables  
+      $RttTotal = $NotRight = 0
+      $TotalResponses = $TotalSites = $Sites.Count 
+      $TimeStamp = Get-Date -Format G 
+      $ReportList = [Collections.ArrayList]@()
+      $PingHTList = [Ordered]@{
+        'DateStamp' = $TimeStamp
+      }
     
-    function Get-PingStatus 
-    {
-      param([Parameter(Mandatory = $true)]$InputObject)
-      switch ($InputObject
-      ) {
-        0  
+      function Get-AverageRTT 
+      {
+        <#
+            .SYNOPSIS
+            Returns the average response times by calculating the information in the $ReceivedJob object
+        #>
+        param
+        (
+          [Parameter(Mandatory = $true, Position = 0)]
+          [Object]$ReceivedJob
+        )
+        $RttTotal = $null
+        ForEach($SiteJob in $ReceivedJob)
         {
-          'Success'
+          $RttTotal += $SiteJob.ResponseTime
         }
-        11003  
-        {
-          'Destination Host Unreachable'
-        }
-        11050  
-        {
-          'General Failure'
-        }
-        Default 
-        {
-          'Unknown Issue'
+        [float]$RttTotal / $ReceivedJob.Count
+      }
+    
+      function Show-PingStatus 
+      {
+        <#
+            .SYNOPSIS
+            Translates the "Status Code" to something meaningful for the user
+        #>
+        param([Parameter(Mandatory = $true)]
+        [int]$InputObject)
+        switch ($InputObject)
+        {      
+          0  
+          {
+            'Success'
+          }
+          11003   
+          {
+            'Destination Host Unreachable'
+          }
+          11050   
+          {
+            'General Failure'
+          }
+          Default 
+          {
+            'Unknown Issue'
+          }
         }
       }
-    }
-    function Get-CurrentLineNumber 
-    {
-      $MyInvocation.ScriptLineNumber
-    } 
-    # Set Variables   
-    $TimeStamp = Get-Date -Format G 
-    $ReportList = [Collections.ArrayList]@()
-    $null = @()
-    $RttTotal = $NotRight = 0
-    $TotalResponses = $TotalSites = $Sites.Count
-    $OutputTable = @{
-      Title  = "`nThe Ping-O-Matic Fiber Tester!"
-      Green  = ' Round Trip Time is GOOD!'
-      Yellow = ' The average is a little high.  An email will be generated to send to the Netowrk team to investigate.'
-      Red    = ' Although not always the case this could indicate that you are on the Satellite.'
-      Report = ''
-    }
+    } # End Begin
+    Process {
+      # Starts the actual "Ping Job" here.  
+      # Then it will receive the job after waiting for it to be completed.  
+      # Removes the jobs from memory
+      $pingjob = Test-Connection -ComputerName $Sites -AsJob -Count 1
+      $ReceivedJob = Receive-Job -Job $pingjob -Wait
+      Remove-Job -Job $pingjob
 
-    $VerboseMsg = @{
-      1 = 'Place Holder Message'
-      2 = 'Log Switch set'
-      3 = 'Test'
+      # Works on the $ReceivedJob to get the data
+      # Adds it to a hash table
+      # Converts the hash table to an Object
+      ForEach($site in $ReceivedJob)
+      {
+        if($site.StatusCode -ne 0)
+        {
+          $TotalResponses = $TotalResponses - 1
+          $NotRight = $NotRight + 1
+        }
+      
+        Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 7 ),'$PingHTList') 
+        $PingHTList.DateStamp = [String]$TimeStamp
+        $PingHTList.Site = [String]$site.Address
+        $PingHTList.IpAddress = [String]$site.IPV4Address  
+        $PingHTList.ResponseTime = [String]$site.ResponseTime
+        $PingHTList.PingResult = (Show-PingStatus -InputObject ($site.StatusCode))
+      
+        Write-Verbose -Message ('{0} - {1} - {2}' -f $site.Address, $site.ResponseTime, $site.StatusCode)
+      
+        # Converts the hash table to object
+        Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 7 ),'$PingHTList') 
+        $null = $ReportList.Add([pscustomobject]$PingHTList)
+      
+      } # End ForEach
+    } # End Process
+    End {
+      $ReportList
     }
+  }
 
-    $PingStat = [Ordered]@{
-      'DateStamp' = $TimeStamp
+  #Testing Below
+  #$Sites = ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com')
+  $Pingresult = Ping-Function -Sites $Sites -Verbose
+
+  function Write-CsvReport
+  {
+    <#
+        .SYNOPSIS
+        Short Description
+    #>
+    [CmdletBinding()]
+    param
+    (
+      [Parameter(Mandatory = $true, Position = 0, HelpMessage = 'Output from the "Ping Function"')]
+      [Object]
+      $InputObject,
+      [Parameter(Mandatory = $false, Position = 1)]
+      $ReportName
+    )
+    #$null = $CsvReportFilename.Replace('.csv',('-{0}.csv' -f (Get-Date -Format yyyy)) )
+  
+    # Set Variables
+    Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 1 ),'$HashTable1, $ArrayList1') 
+    $HashTable1 = [Ordered]@{
+      'DateStamp' = ''
     }
-
-    #region Setup Files
-    If(-not (Test-Path -Path $ReportFile))
-    {
-      $null = New-Item -Path $ReportFile -ItemType File -Force
-    }
-
-    #region CSV File
-    if($ReportCsv) 
+    $ArrayList1 = [Collections.ArrayList]@()
+    # Checking for current file  
+    if($ReportName -and (Test-Path -Path $ReportName) )
     {
       # Trending CSV file setup and site addition
-      Write-Verbose -Message ($LineFormatText -f $(Get-CurrentLineNumber), $VerboseMsg.2)
-      $PingReportInput = Import-Csv -Path $ReportCsv
-      $ColumnNames = ($PingReportInput[0].psobject.Properties).name
+      Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 1 ),'$ImportedData')
+      $ImportedData = Import-Csv -Path $ReportName
+      $ColumnNames = ($ImportedData[0].psobject.Properties).name
       # Add any new sites to the report file
-      foreach($site in $Sites)
+      foreach($site in $InputObject.site)
       {
-        Write-Verbose -Message ($LineFormatText -f $(Get-CurrentLineNumber), $VerboseMsg.1)
-        if(! $ColumnNames.contains($site))
+        Write-Verbose  -Message ('{0}' -f $(Get-CurrentLineNumber -MsgNum 5 )) 
+        if(-not $ColumnNames.contains($site))
         {
-          Write-Verbose -Message ($LineFormatText -f $(Get-CurrentLineNumber), $VerboseMsg.1)
-          $PingReportInput | Add-Member -MemberType NoteProperty -Name $site -Value $null -Force
-          $PingReportInput  | Export-Csv -Path $ReportFile -NoTypeInformation
+          Write-Verbose  -Message ('{0}' -f $(Get-CurrentLineNumber -MsgNum 7 )) 
+          $ImportedData | Add-Member -MemberType NoteProperty -Name $site -Value $null -Force
+          $ImportedData  | Export-Csv -Path $ReportName -NoTypeInformation
         }
       }
-    }do
-     {
-           $ReceivedJob = Receive-Job -Job $pingjob -Wait
-     }
-     until ($x -gt 0)
-     
-    #endregion CSV File
-    # Log file - with monthly rename
-    $OutputTable.Title | Add-Content -Path $ReportFile
-    #endregion Setup Files
-  }
-  Process  {
-    $OutputMessage = 'Output Message'
-    #$ReportList += $OutputMessage   
-    $pingjob = Test-Connection -ComputerName $Sites -AsJob -Count 1
-    $ReceivedJob = Receive-Job -Job $pingjob -Wait
-    ForEach($site in $ReceivedJob)
+    }
+    else
     {
-      if($site.StatusCode -eq 0)
-      {
-        $TotalResponses = $TotalResponses + 1
-        $RttTotal += $site.ResponseTime
-      }
-      else
-      {
-        $TotalResponses = $TotalResponses - 1
-        $NotRight = $NotRight + 1
-      }
-    
-      $PingSucceded = Get-PingStatus -InputObject ($site.StatusCode)
-
-      '{0} - {1} - {2}' -f $site.Address,$site.ResponseTime,$PingSucceded
-      $ReportList += ('{0} - RoundTripTime is {1} ms.' -f $site.Address,$site.ResponseTime) # | Add-Content -Path $ReportFile
- 
-
-      Write-Verbose -Message ($LineFormatText -f $(Get-CurrentLineNumber), $VerboseMsg.1)
-
-    } # End ForEach
-
-$AverageRTT = Get-AverageRTT
-         $ReportList
-  } # End Process
-    
-  <#
-
-      for($i = 0;$i -lt $Sites.Count;$i++)
-      {
-      $site = $Sites[$i] 
-      #$r[$i].Address
-      $RoundTripTime = $ReceivedJob[$i].ResponseTime
-      $PingStatusCode = $ReceivedJob[$i].StatusCode
-      $PingStat[$site] = [string]$RoundTripTime
-
-      $RttTotal += $RoundTripTime
-      
-
-      
-
-
-      
-      }
-
-      $RoundTripTime = $RttTotal/$TotalResponses
-      $OutputTable.Report = ('{1} - {3} tested {0} remote sites and {2} responded. The average response time: {4}ms' -f $TotalSites, $TimeStamp, $TotalResponses, $env:USERNAME, [int]$RoundTripTime) 
-      Write-Verbose -Message $OutputTable.Report
-      #$OutputTable.Report | Out-File $ReportFile
-    
-      If(-Not $Log)
-      {
-      #Write-Output -InputObject $PingStat
-      # Write-Output -InputObject $OutputTable.Report
-      }
-      # Export the hashtable to the file
-      $PingStat |
-      ForEach-Object -Process {
-      [pscustomobject]$_
-      } |     
-      Export-Csv -Path $ReportCsv -NoTypeInformation -Force -Append
-      $LogOutput = (@'
-
-      {1}
-      {0}
-      {3}
-      {2}
-      {1}
-      '@ -f $($OutputTable.Report), ('-' * 30), ('You can find the full report at: {0}' -f $ReportFile),$ReportList)
-      }
-  #>
-
-  End {
+      New-Item -Path $ReportName -ItemType File -Force
+      Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 1 ),'$ReportName') 
+    }
+    # Write Data to HashTable1
+    Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 7 ),'$HashTable1') 
+    $HashTable1.DateStamp = $InputObject[0].DateStamp
+    $InputObject | ForEach-Object -Process {
+      $HashTable1[($_.Site)] = $_.ResponseTime
+    }
+    # Converts the hash table to Custom PS object so that it can "Export-CSV"
+    Write-Verbose  -Message ('{0}' -f $(Get-CurrentLineNumber -MsgNum 1 )) 
+    $null = $ArrayList1.Add([pscustomobject]$HashTable1)
+    # Write "Export" to the CSV file
+    Write-Verbose  -Message ('{0}' -f $(Get-CurrentLineNumber -MsgNum 1 )) 
+    $ArrayList1  |     Export-Csv -Path $ReportName -NoTypeInformation -Force -Append
   }
+
+  # Testing Below
+  $SplatCsvReport = @{
+    InputObject = $Pingresult
+    ReportName  = "$env:HOMEDRIVE\Temp\Reports\FiberSatellite\ReportCsvFunction2.csv"
+    Verbose = $true
+  }
+
+  Write-CsvReport @SplatCsvReport
+
+  function Write-ReportLog
+  {
+    <#
+        .SYNOPSIS
+        Short Description
+    #>
+    [CmdletBinding()]
+    param
+    (
+      [Parameter(Mandatory = $true, Position = 0, HelpMessage = 'Output from the "Ping Function"')]
+      [Object]
+      $InputObject,
+      [Parameter(Mandatory = $false, Position = 1)]
+      $ReportName = 'C:\temp\Reports\FiberSatellite\FiberSatellite.log'
+    )
+  
+    # Set Variables
+    $AverageRTT = $TotalRtt = $TotalResponses = 0
+    $InputObjectcount = $InputObject.count
+    $TimeStamp = Get-Date -Format G
+    $DateStamp = Get-Date -Format yyyy-MMM
+    [String]$ReportFile = $ReportName.Replace('.log',('_{0}.log' -f $DateStamp))
+  
+  
+    $OutputTable = @{
+      Title     = "`nThe Ping-O-Matic Fiber Tester!"
+      TimeStamp = $TimeStamp
+      Green     = ' Round Trip Time is GOOD!'
+      Yellow    = ' The average is a little high.  An email will be generated to send to the Netowrk team to investigate.'
+      Red       = ' Although not always the case this could indicate that you are on the Satellite.'
+      Report    = ''
+    }
+  
+    # Checking for current file  
+    if(-not(Test-Path -Path $ReportFile))
+    {
+      # Log file - with monthly rename
+      Write-Verbose  -Message ('{0} {1}' -f $(Get-CurrentLineNumber -MsgNum 1 ), '$ReportFile')
+      $null = New-Item -Path $ReportFile -ItemType File -Force
+      $OutputTable.Title | Out-File -FilePath $ReportFile -Append
+    }  
+  
+    # Write to log file 
+    $OutputTable.TimeStamp | Out-File -FilePath $ReportFile -Append
+    Foreach($site in $InputObject)
+    {
+      Write-Verbose -Message "Site = $site"
+      #region Math
+      $TotalRtt += ($site.responseTime)
+      if($site.PingResult -eq 'Success')
+      {
+        $TotalResponses ++
+      }
+      #endregion Math       
+      ('{0,-3} ..... {1}' -f $site.ResponseTime, $site.Site) | Tee-Object -FilePath $ReportFile -Append
+    }
+  
+    # Calculate Average RTT
+    $AverageRTT = $TotalRtt/$InputObjectcount
+    Write-Verbose -Message "Average Rtt = $AverageRTT"
+ 
+    # Create the Report bottomlines
+    $OutputTable.Report = (@'
+
+{0,-3} ..... Average Response Time. 
+{1} tested {2} remote sites and {3} responded. 
+You can find the full report at: {4}
+ 
+ {5}
+
+'@ -f $AverageRTT, $env:USERNAME, $InputObjectcount, $TotalResponses, $ReportFile, ('-' * 30))
+
+    #Write-Verbose -Message $OutputTable.Report
+    $OutputTable.Report  | Tee-Object -FilePath $ReportFile -Append
+
+    #| Out-File -FilePath $ReportFile -Append
+    # Start-Process -FilePath notepad -ArgumentList $ReportFile
+  }
+  
+  #Testing Below
+  $SplatLogReport = @{
+    InputObject = $Pingresult
+    ReportName  = "C:\Temp\Reports\FiberSatellite\Fiber-Satellite.log"
+    Verbose = $true
+  }
+  
+  Write-ReportLog @SplatLogReport 
 }
 
-
-
-
-$DailySplat = @{
-  'Log'      = $true
-  'ReportCsv' = 'C:\Temp\Reports\FiberSatellite\Ping.csv'
-  'Reportfile' = 'C:\temp\Reports\FiberSatellite\Ping.log'
-  'Sites'    = ('localhost', 'www.google.com', 'www.bing.com', 'www.yahoo.com')
-  'Verbose'  = $true
-}
-# For Testing:
-$ReportList = Test-FiberSatellite @DailySplat
-#Test-FiberSatellite -Simple
-#Test-FiberSatellite -Sites localhost,'yahoo.com'
-#Test-FiberSatellite -Sites localhost,'yahoo.com' -Simple 
-#Test-FiberSatellite -Sites localhost,'yahoo.com' -Simple -Verbose
-#Test-FiberSatellite -Log -ReportFolder C:\Temp
-#Test-FiberSatellite -Log -Verbose
+Test-FiberSatellite -Sites ('localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com')
