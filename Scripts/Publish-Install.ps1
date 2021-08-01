959a81be0e6811d8573d9c108b506ac50e925a2e
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-08-01 10:26:09 -0500

    Create Publish-Install.ps1

---
+++ Scripts/Publish-Install.ps1
@@ -0,0 +1,33 @@
+﻿#!/usr/bin/env powershell
+#requires -Version 2.0 -Modules PowerShellGet
+
+$PSGallery = 'NasPSGallery'
+$ModuleName = 'ITPS.OMCS.Tools'
+
+if(Test-Path -Path $env:USERPROFILE\Documents\GitHub)
+{
+  $ModuleLocation = "$env:USERPROFILE\Documents\GitHub\"
+}
+elseif(Test-Path -Path D:\GitHub\KnarrStudio)
+{
+  $ModuleLocation = 'D:\GitHub\KnarrStudio\'
+}
+Set-Location -Path $ModuleLocation
+
+$PublishSplat = @{
+  Name       = ('{0}\{1}' -f $ModuleLocation, $ModuleName)
+  Repository = $PSGallery
+}
+
+$InstallSplat = @{
+  Name         = $ModuleName
+  Repository   = $PSGallery
+  Scope        = 'CurrentUser'
+  AllowClobber = $true
+  Force        = $true
+}
+
+
+Publish-Module @PublishSplat 
+#Install-Module @InstallSplat
+
