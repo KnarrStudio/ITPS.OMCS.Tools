25ce0e4a074bc137564657e7a02c053248f7f891
Erik JA <OgJAkFy8@users.noreply.github.com>
2025-09-20 11:31:23 -0500

    deleted copy folder

--- Copy/init - Copy.ps1
+++
@@ -1,9 +0,0 @@
-﻿#requires -Version 1.0
-
-# use this file to define global variables on module scope
-# or perform other initialization procedures.
-# this file will not be touched when new functions are exported to
-# this module.
-
-
-
